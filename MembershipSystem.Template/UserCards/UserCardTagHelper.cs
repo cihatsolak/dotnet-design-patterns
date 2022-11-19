d66f9eeda536e1a1bb7265c048de06ef1119dd72
Cihat SOLAK <54249736+cihatsolak@users.noreply.github.com>
2022-11-20 01:15:38 +0300

    Create user card tag helper

---
+++ MembershipSystem.Template/UserCards/UserCardTagHelper.cs
@@ -0,0 +1,36 @@
+﻿namespace MembershipSystem.Template.UserCards
+{
+    public class UserCardTagHelper : TagHelper
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly UserManager<AppUser> _userManager;
+
+        public UserCardTagHelper(
+            IHttpContextAccessor httpContextAccessor, 
+            UserManager<AppUser> userManager)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _userManager = userManager;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            UserCardTemplate userCardTemplate;
+
+            if (_httpContextAccessor.HttpContext.User.Identity.IsAuthenticated)
+            {
+                userCardTemplate = new PrimeUserCardTemplate();
+            }
+            else
+            {
+                userCardTemplate = new DefaultUserCardTemplate();
+            }
+
+            var user = _userManager.FindByNameAsync(_httpContextAccessor.HttpContext.User.Identity.Name).Result;
+
+            userCardTemplate.SetUser(user);
+
+            output.Content.SetHtmlContent(userCardTemplate.Build());
+        }
+    }
+}
