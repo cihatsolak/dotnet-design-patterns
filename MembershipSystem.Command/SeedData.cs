3208a4d90aca459a0c84b8976fae8727940a2fd6
Cihat SOLAK <54249736+cihatsolak@users.noreply.github.com>
2022-11-20 14:26:07 +0300

    Create database structure and add sample data

--- MembershipSystem.Command/SeedData.cs
+++ MembershipSystem.Command/SeedData.cs
@@ -2,7 +2,7 @@
 {
     public static class SeedData
     {
-        public static void AddSeedData(this WebApplicationBuilder builder)
+        public static async void AddSeedData(this WebApplicationBuilder builder)
         {
             var serviceProvider = builder.Services.BuildServiceProvider();
             var identityDbContext = serviceProvider.GetRequiredService<AppIdentityDbContext>();
@@ -41,6 +41,13 @@
                     UserName = "user5",
                     Email = "user5@hotmail.com",
                 }, "Password12*").Wait();
+
+                Enumerable.Range(1, 30).ToList().ForEach(x =>
+                {
+                    identityDbContext.Products.Add(new Product() { Name = $"kalem {x}", Price = x * 100, Stock = x + 50 });
+                });
+
+                identityDbContext.SaveChangesAsync().Wait();
             }
         }
     }
--- MembershipSystem.Command/Usings.cs
+++ MembershipSystem.Command/Usings.cs
@@ -4,3 +4,6 @@ global using Microsoft.AspNetCore.Identity;
 global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Internal;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
